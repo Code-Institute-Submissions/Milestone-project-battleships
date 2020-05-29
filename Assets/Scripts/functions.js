fb31350b9708a1c0dbc55344829faac0b7236a53
Lindsaykerr1994 <lindsaykerr1994@gmail.com>
2020-05-29 17:08:44 +0000

    Created 'getOpponentCoordinates' and 'checkReadyStatus' functions. This will upload a set of coordinates for the opponent, and see if the user has successfully placed all of their ships

--- Assets/Scripts/functions.js
+++ Assets/Scripts/functions.js
@@ -1,4 +1,5 @@
 var shipSelection = [["Aircraft Carrier",5],["Battleship",4],["Submarine",3],["Destroyer",3],["Patrol Boat",2]];
+
 var newScreen = function(){
     //This clears the squares, providing a new board to play upon
     $(".game-square").removeClass("occupied");
@@ -13,6 +14,8 @@ var newScreen = function(){
     $("#submarine").click(placeSubmarine);
     $("#destroyer").click(placeDestroyer);
     $("#patrolboat").click(placePatrolBoat);
+    
+
 }
 //This function allows the user to place a selected ship.
 function placeAircraftCarrier(){
@@ -37,7 +40,8 @@ function placeAircraftCarrier(){
                 $("#message-panel-2 p").html("Select another ship");
                 $("#aircraftcarrier").addClass("placed");
             }
-        }) ;
+            
+        }); 
     }
 }
 function placeBattleship(){
@@ -85,7 +89,7 @@ function placeSubmarine(){
                 }
             } else {
                 $("#message-panel-2 p").html("Select another ship");
-                $("#battleship").addClass("placed");
+                $("#submarine").addClass("placed");
             }
         }) ;
     }
@@ -137,9 +141,102 @@ function placePatrolBoat(){
                 $("#message-panel-2 p").html("Select another ship");
                 $("#patrolboat").addClass("placed");
             }
-        }) ;
+        });
     }
 }
+function getOpponentCoordinates(){
+    if(checkReadyStatus() == true){
+    $.get("board-1.txt",function(rawCoor){
+        var oppCoor = rawCoor.split(/\n/g);
+        var ACCoor = oppCoor[0].split(",");
+        for(i=0;i<ACCoor.length;i++){
+            var currentCoor = ACCoor[i];
+            console.log(currentCoor);
+            $(`#opp-game-board .${currentCoor}`).addClass("occupied");
+        }
+        var BSCoor = oppCoor[1].split(",");
+        for(i=0;i<BSCoor.length;i++){
+            var currentCoor = BSCoor[i];
+            console.log(currentCoor);
+            $(`#opp-game-board .${currentCoor}`).addClass("occupied");
+        }
+        var SubCoor = oppCoor[2].split(",");
+        for(i=0;i<SubCoor.length;i++){
+            var currentCoor = SubCoor[i];
+            console.log(currentCoor);
+            $(`#opp-game-board .${currentCoor}`).addClass("occupied");
+        }
+        var DesCoor = oppCoor[3].split(",");
+        for(i=0;i<DesCoor.length;i++){
+            var currentCoor = DesCoor[i];
+            console.log(currentCoor);
+            $(`#opp-game-board .${currentCoor}`).addClass("occupied");
+        }
+        var PBCoor = oppCoor[4].split(",");
+        for(i=0;i<PBCoor.length;i++){
+            var currentCoor = PBCoor[i];
+            console.log(currentCoor);
+            $(`#opp-game-board .${currentCoor}`).addClass("occupied");
+        }
+
+    })
+    } else {
+        console.log("You're a poop head");
+    }
+}
+function checkReadyStatus(readyStatus){
+    var readyStatus
+    var aircraftReady;
+    var battleshipReady;
+    var submarineReady;
+    var destroyerReady;
+    var patrolboatReady;
+    if($("#aircraftcarrier").hasClass("placed")){
+        aircraftReady = true;
+        console.log("Aircraft Carrier Ready");
+    } else {
+        aircraftReady = false;
+        console.log("Please deploy your Aircraft Carrier");
+    }
+    if($("#battleship").hasClass("placed")){
+        battleshipReady = true;
+        console.log("Battleship Ready");
+    } else {
+        battleshipReady = false;
+        console.log("Please deploy your Battleship");
+    }
+    if($("#submarine").hasClass("placed")){
+        submarineReady = true;
+        console.log("Submarine Ready");
+    } else {
+        submarineReady = false;
+        console.log("Please deploy your Submarine");
+    }
+    if($("#destroyer").hasClass("placed")){
+        destroyerReady = true;
+        console.log("Destroyer Ready");
+    } else {
+        destroyerReady = false;
+        console.log("Please deploy your Destroyer");
+    }
+    if($("#patrolboat").hasClass("placed")){
+        patrolboatReady = true;
+        console.log("Patrol Boat Ready");
+    } else {
+        patrolboatReady = false;
+        console.log("Please deploy your Patrol Boat");
+    }
+    if(aircraftReady == true && battleshipReady == true && submarineReady == true && destroyerReady == true && patrolboatReady == true){
+        console.log("All ships deployed");
+        readyStatus = true;
+    } else {
+        console.log("Please place all your ships before starting");
+        readyStatus = false;
+        
+    }
+    return readyStatus;
+}
+
 /*function checkOccupiedSquare(spacesRemaining){
     var spacesRemaining;
             if(spacesRemaining > 0){
@@ -155,9 +252,11 @@ function placePatrolBoat(){
             }
 }*/
 
+$("#new-game-btn").click(newScreen);
+$("#ready-btn").click(getOpponentCoordinates);
 
-//This function checks if selected square is occupied or not.
-$("#play-btn").click(newScreen);
+
+
     
 
 
